17d06b5d654b296beaa5812e1a925b73f5aa2b7b
Ramnatthan Alagappan <ra@cs.wisc.edu>
2016-11-10 15:27:04 -0600

    Add alice-strace framework -- this is the underlying core tool that helps trace all events in the system

---
+++ alice-strace/config.h
@@ -0,0 +1,557 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define for the AArch64 architecture. */
+/* #undef AARCH64 */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Define for the Alpha architecture. */
+/* #undef ALPHA */
+
+/* Define for the ARM architecture. */
+/* #undef ARM */
+
+/* Define for the AVR32 architecture. */
+/* #undef AVR32 */
+
+/* Define for the Blackfin architecture. */
+/* #undef BFIN */
+
+/* Define for the CRISv10 architecture. */
+/* #undef CRISV10 */
+
+/* Define for the CRISv32 architecture. */
+/* #undef CRISV32 */
+
+/* Define to the type of elements in the array set by `getgroups'. Usually
+   this is either `int' or `gid_t'. */
+#define GETGROUPS_T gid_t
+
+/* Define to 1 if you have the <asm/cachectl.h> header file. */
+/* #undef HAVE_ASM_CACHECTL_H */
+
+/* Define to 1 if you have the <asm/sigcontext.h> header file. */
+/* #undef HAVE_ASM_SIGCONTEXT_H */
+
+/* Define to 1 if you have the <asm/sysmips.h> header file. */
+/* #undef HAVE_ASM_SYSMIPS_H */
+
+/* Define to 1 if you have BLKGETSIZE64. */
+#define HAVE_BLKGETSIZE64 1
+
+/* Define to 1 if you have the declaration of `IO_CMD_PWRITE', and to 0 if you
+   don't. */
+/* #undef HAVE_DECL_IO_CMD_PWRITE */
+
+/* Define to 1 if you have the declaration of `IO_CMD_PWRITEV', and to 0 if
+   you don't. */
+/* #undef HAVE_DECL_IO_CMD_PWRITEV */
+
+/* Define to 1 if you have the declaration of `LO_FLAGS_AUTOCLEAR', and to 0
+   if you don't. */
+#define HAVE_DECL_LO_FLAGS_AUTOCLEAR 1
+
+/* Define to 1 if you have the declaration of `LO_FLAGS_PARTSCAN', and to 0 if
+   you don't. */
+#define HAVE_DECL_LO_FLAGS_PARTSCAN 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_CLONE', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_EVENT_CLONE 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_EXEC', and to 0 if
+   you don't. */
+#define HAVE_DECL_PTRACE_EVENT_EXEC 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_EXIT', and to 0 if
+   you don't. */
+#define HAVE_DECL_PTRACE_EVENT_EXIT 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_FORK', and to 0 if
+   you don't. */
+#define HAVE_DECL_PTRACE_EVENT_FORK 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_VFORK', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_EVENT_VFORK 1
+
+/* Define to 1 if you have the declaration of `PTRACE_EVENT_VFORK_DONE', and
+   to 0 if you don't. */
+#define HAVE_DECL_PTRACE_EVENT_VFORK_DONE 1
+
+/* Define to 1 if you have the declaration of `PTRACE_GETEVENTMSG', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_GETEVENTMSG 1
+
+/* Define to 1 if you have the declaration of `PTRACE_GETSIGINFO', and to 0 if
+   you don't. */
+#define HAVE_DECL_PTRACE_GETSIGINFO 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACECLONE', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACECLONE 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACEEXEC', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACEEXEC 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACEEXIT', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACEEXIT 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACEFORK', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACEFORK 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACESYSGOOD', and to
+   0 if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACESYSGOOD 1
+
+/* Define to 1 if you have the declaration of `PTRACE_O_TRACEVFORK', and to 0
+   if you don't. */
+#define HAVE_DECL_PTRACE_O_TRACEVFORK 1
+
+/* Define to 1 if you have the declaration of `PTRACE_SETOPTIONS', and to 0 if
+   you don't. */
+#define HAVE_DECL_PTRACE_SETOPTIONS 1
+
+/* Define to 1 if you have the declaration of `sys_errlist', and to 0 if you
+   don't. */
+#define HAVE_DECL_SYS_ERRLIST 1
+
+/* Define to 1 if you have the declaration of `sys_siglist', and to 0 if you
+   don't. */
+#define HAVE_DECL_SYS_SIGLIST 1
+
+/* Define to 1 if you have the declaration of `_sys_siglist', and to 0 if you
+   don't. */
+#define HAVE_DECL__SYS_SIGLIST 1
+
+/* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
+   */
+#define HAVE_DIRENT_H 1
+
+/* Define to 1 if you have the <elf.h> header file. */
+#define HAVE_ELF_H 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the `if_indextoname' function. */
+#define HAVE_IF_INDEXTONAME 1
+
+/* Define to 1 if you have the `inet_ntop' function. */
+#define HAVE_INET_NTOP 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <ioctls.h> header file. */
+/* #undef HAVE_IOCTLS_H */
+
+/* Define to 1 if you have the <libaio.h> header file. */
+/* #undef HAVE_LIBAIO_H */
+
+/* Define to 1 if you have the <linux/capability.h> header file. */
+#define HAVE_LINUX_CAPABILITY_H 1
+
+/* Define to 1 if you have the <linux/icmp.h> header file. */
+#define HAVE_LINUX_ICMP_H 1
+
+/* Define to 1 if you have the <linux/if_packet.h> header file. */
+#define HAVE_LINUX_IF_PACKET_H 1
+
+/* Define to 1 if you have the <linux/in6.h> header file. */
+#define HAVE_LINUX_IN6_H 1
+
+/* Define to 1 if you have the <linux/netlink.h> header file. */
+#define HAVE_LINUX_NETLINK_H 1
+
+/* Define to 1 if you have the <linux/perf_event.h> header file. */
+#define HAVE_LINUX_PERF_EVENT_H 1
+
+/* Define to 1 if you have the <linux/ptrace.h> header file. */
+#define HAVE_LINUX_PTRACE_H 1
+
+/* Define to 1 if you have the <linux/utsname.h> header file. */
+#define HAVE_LINUX_UTSNAME_H 1
+
+/* Define if long long is little-endian. */
+#define HAVE_LITTLE_ENDIAN_LONG_LONG 1
+
+/* Define to 1 if the system has the type `long long'. */
+#define HAVE_LONG_LONG 1
+
+/* Define if off_t is a long long. */
+/* #undef HAVE_LONG_LONG_OFF_T */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <mqueue.h> header file. */
+#define HAVE_MQUEUE_H 1
+
+/* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
+/* #undef HAVE_NDIR_H */
+
+/* Define to 1 if you have the <netinet/sctp.h> header file. */
+/* #undef HAVE_NETINET_SCTP_H */
+
+/* Define to 1 if you have the <netinet/tcp.h> header file. */
+#define HAVE_NETINET_TCP_H 1
+
+/* Define to 1 if you have the <netinet/udp.h> header file. */
+#define HAVE_NETINET_UDP_H 1
+
+/* Define to 1 if you have the <poll.h> header file. */
+#define HAVE_POLL_H 1
+
+/* Define to 1 if you have the `prctl' function. */
+#define HAVE_PRCTL 1
+
+/* Define to 1 if you have the `process_vm_readv' function. */
+#define HAVE_PROCESS_VM_READV 1
+
+/* Define to 1 if you have the `sendmsg' function. */
+#define HAVE_SENDMSG 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define to 1 if the system has the type `siginfo_t'. */
+#define HAVE_SIGINFO_T 1
+
+/* Define to 1 if the system has the type `sig_atomic_t'. */
+#define HAVE_SIG_ATOMIC_T 1
+
+/* Define if stat64 is available in asm/stat.h. */
+/* #undef HAVE_STAT64 */
+
+/* Define if statfs64 is available in sys/vfs.h. */
+#define HAVE_STATFS64 1
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `stpcpy' function. */
+#define HAVE_STPCPY 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <stropts.h> header file. */
+#define HAVE_STROPTS_H 1
+
+/* Define to 1 if you have the `strsignal' function. */
+#define HAVE_STRSIGNAL 1
+
+/* Define to 1 if `dqb_curblocks' is a member of `struct dqblk'. */
+/* #undef HAVE_STRUCT_DQBLK_DQB_CURBLOCKS */
+
+/* Define to 1 if the system has the type `struct ia64_fpreg'. */
+/* #undef HAVE_STRUCT_IA64_FPREG */
+
+/* Define to 1 if `u.c.flags' is a member of `struct iocb'. */
+/* #undef HAVE_STRUCT_IOCB_U_C_FLAGS */
+
+/* Define to 1 if `msg_control' is a member of `struct msghdr'. */
+#define HAVE_STRUCT_MSGHDR_MSG_CONTROL 1
+
+/* Define to 1 if the system has the type `struct opthdr'. */
+/* #undef HAVE_STRUCT_OPTHDR */
+
+/* Define to 1 if the system has the type `struct pt_all_user_regs'. */
+/* #undef HAVE_STRUCT_PT_ALL_USER_REGS */
+
+/* Define to 1 if the system has the type `struct sigcontext'. */
+#define HAVE_STRUCT_SIGCONTEXT 1
+
+/* Define to 1 if `sc_hi2' is a member of `struct sigcontext'. */
+/* #undef HAVE_STRUCT_SIGCONTEXT_SC_HI2 */
+
+/* Define to 1 if the system has the type `struct sigcontext_struct'. */
+/* #undef HAVE_STRUCT_SIGCONTEXT_STRUCT */
+
+/* Define to 1 if `sin6_scope_id' is a member of `struct sockaddr_in6'. */
+#define HAVE_STRUCT_SOCKADDR_IN6_SIN6_SCOPE_ID 1
+
+/* Define to 1 if `st_aclcnt' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_ACLCNT */
+
+/* Define to 1 if `st_blksize' is a member of `struct stat'. */
+#define HAVE_STRUCT_STAT_ST_BLKSIZE 1
+
+/* Define to 1 if `st_blocks' is a member of `struct stat'. */
+#define HAVE_STRUCT_STAT_ST_BLOCKS 1
+
+/* Define to 1 if `st_flags' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_FLAGS */
+
+/* Define to 1 if `st_fstype' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_FSTYPE */
+
+/* Define to 1 if `st_gen' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_GEN */
+
+/* Define to 1 if `st_level' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_LEVEL */
+
+/* Define to 1 if `st_rdev' is a member of `struct stat'. */
+#define HAVE_STRUCT_STAT_ST_RDEV 1
+
+/* Define to 1 if `ACCEPTOR_id' is a member of `struct T_conn_res'. */
+/* #undef HAVE_STRUCT_T_CONN_RES_ACCEPTOR_ID */
+
+/* Define to 1 if `QUEUE_ptr' is a member of `struct T_conn_res'. */
+/* #undef HAVE_STRUCT_T_CONN_RES_QUEUE_PTR */
+
+/* Define to 1 if the system has the type `struct t_opthdr'. */
+/* #undef HAVE_STRUCT_T_OPTHDR */
+
+/* Define to 1 if the system has the type `struct user_desc'. */
+#define HAVE_STRUCT_USER_DESC 1
+
+/* Define to 1 if `domainname' is a member of `struct utsname'. */
+#define HAVE_STRUCT_UTSNAME_DOMAINNAME 1
+
+/* Define to 1 if the system has the type `struct __old_kernel_stat'. */
+#define HAVE_STRUCT___OLD_KERNEL_STAT 1
+
+/* Define to 1 if you have the <sys/acl.h> header file. */
+/* #undef HAVE_SYS_ACL_H */
+
+/* Define to 1 if you have the <sys/asynch.h> header file. */
+/* #undef HAVE_SYS_ASYNCH_H */
+
+/* Define to 1 if you have the <sys/conf.h> header file. */
+/* #undef HAVE_SYS_CONF_H */
+
+/* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define to 1 if you have the <sys/epoll.h> header file. */
+#define HAVE_SYS_EPOLL_H 1
+
+/* Define to 1 if you have the <sys/filio.h> header file. */
+/* #undef HAVE_SYS_FILIO_H */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
+   */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define to 1 if you have the <sys/poll.h> header file. */
+#define HAVE_SYS_POLL_H 1
+
+/* Define to 1 if you have the <sys/ptrace.h> header file. */
+#define HAVE_SYS_PTRACE_H 1
+
+/* Define to 1 if you have the <sys/reg.h> header file. */
+#define HAVE_SYS_REG_H 1
+
+/* Define to 1 if you have the `sys_siglist' function. */
+#define HAVE_SYS_SIGLIST 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/uio.h> header file. */
+#define HAVE_SYS_UIO_H 1
+
+/* Define to 1 if you have the <sys/vfs.h> header file. */
+#define HAVE_SYS_VFS_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Define to 1 if you have the `_sys_siglist' function. */
+#define HAVE__SYS_SIGLIST 1
+
+/* Define for the HPPA architecture. */
+/* #undef HPPA */
+
+/* Define for the i386 architecture. */
+/* #undef I386 */
+
+/* Define for the IA64 architecture. */
+/* #undef IA64 */
+
+/* Define for the m68k architecture. */
+/* #undef M68K */
+
+/* Define to 1 if `major', `minor', and `makedev' are declared in <mkdev.h>.
+   */
+/* #undef MAJOR_IN_MKDEV */
+
+/* Define to 1 if `major', `minor', and `makedev' are declared in
+   <sysmacros.h>. */
+/* #undef MAJOR_IN_SYSMACROS */
+
+/* Define for the Meta architecture. */
+/* #undef METAG */
+
+/* Define for the MicroBlaze architecture. */
+/* #undef MICROBLAZE */
+
+/* Define for the MIPS architecture. */
+/* #undef MIPS */
+
+/* Define for the OpenRISC 1000 architecture. */
+/* #undef OR1K */
+
+/* Name of package */
+#define PACKAGE "alice-strace"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "alice-strace-devel@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "alice-strace"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "alice-strace 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "alice-strace"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Define for the PowerPC architecture. */
+/* #undef POWERPC */
+
+/* Define for the PowerPC64 architecture. */
+/* #undef POWERPC64 */
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Define for the S390 architecture. */
+/* #undef S390 */
+
+/* Define for the S390x architecture. */
+/* #undef S390X */
+
+/* Define for the SH architecture. */
+/* #undef SH */
+
+/* Define for the SH64 architecture. */
+/* #undef SH64 */
+
+/* The size of `long', as computed by sizeof. */
+#define SIZEOF_LONG 8
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* The size of `rlim_t', as computed by sizeof. */
+#define SIZEOF_RLIM_T 8
+
+/* Define for the SPARC architecture. */
+/* #undef SPARC */
+
+/* Define for the SPARC64 architecture. */
+/* #undef SPARC64 */
+
+/* Define to 1 if the `S_IS*' macros in <sys/stat.h> do not work properly. */
+/* #undef STAT_MACROS_BROKEN */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define for the Tile architecture */
+/* #undef TILE */
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Version number of package */
+#define VERSION "0.1"
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define for the 32bit AMD x86-64 architecture. */
+/* #undef X32 */
+
+/* Define for the 64bit AMD x86-64 architecture. */
+#define X86_64 1
+
+/* Define for the Xtensa architecture */
+/* #undef XTENSA */
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
